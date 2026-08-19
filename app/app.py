90b4c27aaada4b1c968e5ab73ec5f24ecda51f36
Hidayah <60766668+nhidayahj@users.noreply.github.com>
2026-08-20 02:43:13 +0800

    added first courses db with mock entries, modify endpoint routes, update api specs

--- app/app.py
+++ app/app.py
@@ -1,5 +1,5 @@
 import os
-
+import sqlite3
 from flask import Flask, jsonify, request
 
 # Creates a Flask application
@@ -7,17 +7,23 @@ app = Flask(__name__)
 
 app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
 
-# Test API routes
+# create SQLite connection
+def get_db_connection():
+    db = sqlite3.connect("instance/courses.db")
+    db.row_factory = sqlite3.Row
+    return db
 
-# Sends a GET request
-@app.get('/api/greet')
-def greet():
-    return "Welcome first connection"
 
-
-@app.get('/api/courses')
+# GET ALL COURSES
+@app.route('/api/v1/courses', methods=["GET"])
 def get_courses():
-    return jsonify({"message": "GET /api/courses works!"})
+    db = get_db_connection()
+    courses = db.execute(
+        'SELECT * FROM courses'
+    ).fetchall()
+    print(f"courses:{courses}")
+    db.close()
+    return [dict(course) for course in courses]
 
 if __name__ == "__main__":
     app.run(debug=True)
