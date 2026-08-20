59aeffe4abd1bc59d74ae66eaf630ee2edd410b2
Hidayah <60766668+nhidayahj@users.noreply.github.com>
2026-08-20 15:13:07 +0800

    added retieve course by Id endpoint route

--- app/app.py
+++ app/app.py
@@ -18,12 +18,33 @@ def get_db_connection():
 @app.route('/api/v1/courses', methods=["GET"])
 def get_courses():
     db = get_db_connection()
-    courses = db.execute(
+    cursor = db.cursor()
+
+    # Execute the query
+    courses = cursor.execute(
         'SELECT * FROM courses'
     ).fetchall()
-    print(f"courses:{courses}")
     db.close()
     return [dict(course) for course in courses]
 
+# RETRIEVE INDIV COURSE BY COURSE_ID
+@app.route('/api/v1/courses/<string:course_id>', methods=["GET"])
+def get_one_course(course_id): 
+
+    db = get_db_connection()
+    cursor = db.cursor()
+    course = cursor.execute(
+        'SELECT * FROM courses WHERE course_id = ?', 
+        (course_id,)
+    ).fetchone()
+    db.close()
+
+    return jsonify(dict(course)), 200
+
+
+
 if __name__ == "__main__":
+    # Below allows Flask to be automatically reloaded when changes made and saved
     app.run(debug=True)
+    # Command below to run Flask application
+    # Command: flask --app app.run --debug
